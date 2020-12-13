06538bfa52530a7ec07dbdfa5b7744c87ed70f42
Lars Lorenz <lars.ll.lorenz@gmail.com>
2020-12-13 01:05:55 +0100

    Add Save button

--- app/src/main/java/com/example/howie/AddTaskActivity.kt
+++ app/src/main/java/com/example/howie/AddTaskActivity.kt
@@ -2,12 +2,20 @@ package com.example.howie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_add_task.*
 
 class AddTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
-        setSupportActionBar(findViewById(R.id.my_toolbar))
+        setSupportActionBar(toolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_add_task, menu)
+        return true
     }
 }
