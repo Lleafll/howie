2b424055ce77fb75e9beda0b81b2d8f60c9d0e08
Lars Lorenz <lars.ll.lorenz@gmail.com>
2020-12-17 17:00:24 +0100

    Fix comparison

--- app/src/main/java/com/example/howie/TaskAdapter.kt
+++ app/src/main/java/com/example/howie/TaskAdapter.kt
@@ -33,10 +33,10 @@ class TaskAdapter(private val clickListener: (Task) -> Unit) :
 
 
     class TasksComparator : DiffUtil.ItemCallback<Task>() {
-        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean = oldItem === newItem
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean =
+            oldItem.id == newItem.id
 
         override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean = oldItem == newItem
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder =
