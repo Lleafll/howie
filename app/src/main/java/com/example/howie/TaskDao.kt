00cdca22c013e43d4a7fbf3488b20ffdbacdaff7
Lars Lorenz <lars.ll.lorenz@gmail.com>
2020-12-20 00:15:23 +0100

    Use compound queries for database

--- app/src/main/java/com/example/howie/TaskDao.kt
+++ app/src/main/java/com/example/howie/TaskDao.kt
@@ -3,21 +3,32 @@ package com.example.howie
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
+private const val isImportant = "importance = 0"
+private const val isUnimportant = "importance = 0"
+private const val isDue = "due IS NOT NULL"
+private const val isNotDue = "due IS NULL"
+private const val isSnoozed = "snoozed IS NOT NULL AND snoozed > due"
+private const val isNotSnoozed = "snoozed IS NULL OR snoozed <= due"
+private const val order = "ORDER BY snoozed, due"
+
 @Dao
 interface TaskDao {
     @Query("SELECT * FROM task")
     fun getAllTasks(): LiveData<List<Task>>
 
-    @Query("SELECT * FROM task WHERE importance = 0 AND due IS NOT NULL ORDER BY snoozed, due")
+    @Query("SELECT * FROM task WHERE $isImportant AND $isDue AND $isNotSnoozed $order")
     fun getDoTasks(): LiveData<List<Task>>
 
-    @Query("SELECT * FROM task WHERE importance = 0 AND due IS NULL ORDER BY snoozed")
+    @Query("SELECT * FROM task WHERE $isImportant AND $isDue AND $isSnoozed $order")
+    fun getSnoozedDoTasks(): LiveData<List<Task>>
+
+    @Query("SELECT * FROM task WHERE $isImportant AND $isNotDue $order")
     fun getDecideTasks(): LiveData<List<Task>>
 
-    @Query("SELECT * FROM task WHERE importance = 1 AND due IS NOT NULL ORDER BY snoozed, due")
+    @Query("SELECT * FROM task WHERE $isUnimportant AND $isDue $order")
     fun getDelegateTasks(): LiveData<List<Task>>
 
-    @Query("SELECT * FROM task WHERE importance = 1 AND due IS NULL ORDER BY snoozed")
+    @Query("SELECT * FROM task WHERE $isUnimportant AND $isNotDue $order")
     fun getDropTasks(): LiveData<List<Task>>
 
     @Query("SELECT * FROM task WHERE id = :id")
