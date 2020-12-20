373254d5620da1efffc4823986660530304b6010
Lars Lorenz <lars.ll.lorenz@gmail.com>
2020-12-20 22:54:55 +0100

    Restyle TaskView

--- app/src/main/java/com/example/howie/TaskView.kt
+++ app/src/main/java/com/example/howie/TaskView.kt
@@ -2,10 +2,10 @@ package com.example.howie
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.task_view.view.*
 import java.time.LocalDate
@@ -17,43 +17,48 @@ class TaskView : ConstraintLayout {
     init {
         inflate(context, R.layout.task_view, this)
         snoozeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            snoozePicker.isVisible = isChecked
+            snoozedTextDate.isVisible = isChecked
         }
         dueSwitch.setOnCheckedChangeListener { _, isChecked ->
-            duePicker.isVisible = isChecked
+            dueTextDate.isVisible = isChecked
         }
     }
 
     fun setTask(task: Task) {
         taskNameEditText.setText(task.name)
-        importanceButton.isChecked = task.importance == Importance.IMPORTANT
-        setDateFields(duePicker, dueSwitch, task.due)
-        setDateFields(snoozePicker, snoozeSwitch, task.snoozed)
+        if(task.importance == Importance.IMPORTANT) {
+            importantButton.isChecked = true
+        } else {
+            unimportantButton.isChecked = true
+        }
+        setDateFields(dueTextDate, dueSwitch, task.due)
+        setDateFields(snoozedTextDate, snoozeSwitch, task.snoozed)
     }
 
     fun getTask() = Task(
         taskNameEditText.text.toString(),
-        if (importanceButton.isChecked) Importance.IMPORTANT else Importance.UNIMPORTANT,
-        readDate(dueSwitch, duePicker),
-        readDate(snoozeSwitch, snoozePicker)
+        if (importantButton.isChecked) Importance.IMPORTANT else Importance.UNIMPORTANT,
+        readDate(dueSwitch, dueTextDate),
+        readDate(snoozeSwitch, snoozedTextDate)
     )
 }
 
-private fun setDateFields(picker: DatePicker, switch: SwitchCompat, date: LocalDate?) {
+private fun setDateFields(picker: TextView, switch: SwitchCompat, date: LocalDate?) {
     if (date != null) {
         switch.isChecked = true
         picker.isVisible = true
-        picker.updateDate(date.year, date.monthValue - 1, date.dayOfMonth)
+        picker.text = date.toString()
     } else {
         switch.isChecked = false
         picker.isVisible = false
+        picker.text = LocalDate.now().toString()
     }
 }
 
-private fun readDate(switch: SwitchCompat, picker: DatePicker): LocalDate? {
+private fun readDate(switch: SwitchCompat, picker: EditText): LocalDate? {
     return if (!switch.isChecked) {
         null
     } else {
-        LocalDate.of(picker.year, picker.month + 1, picker.dayOfMonth)
+        LocalDate.now() // TODO(Implement)
     }
 }
