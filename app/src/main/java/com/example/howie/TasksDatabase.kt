c0c9781c54edb5b53f1205496c294fe89368ac33
Lars Lorenz <lars.ll.lorenz@gmail.com>
2020-12-21 21:05:05 +0100

    Add taskListId to Task

--- app/src/main/java/com/example/howie/TasksDatabase.kt
+++ app/src/main/java/com/example/howie/TasksDatabase.kt
@@ -7,15 +7,20 @@ import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(version = 5, entities = (arrayOf(Task::class)))
+@Database(version = 6, entities = (arrayOf(Task::class)))
 abstract class TasksDatabase : RoomDatabase() {
     abstract fun getTaskDao(): TaskDao
-
 }
 
 val MIGRATION_4_5 = object : Migration(4, 5) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE Task ADD archived INTEGER")
+    }
+}
+
+val MIGRATION_5_6 = object : Migration(4, 5) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE Task ADD taskListId INTEGER NOT NULL DEFAULT 0")
     }
 }
 
@@ -28,7 +33,7 @@ object TasksDatabaseSingleton {
                 applicationContext,
                 TasksDatabase::class.java,
                 "tasks-database"
-            ).addMigrations(MIGRATION_4_5).build()
+            ).addMigrations(MIGRATION_4_5, MIGRATION_5_6).build()
         }
         return database!!
     }
