f75d39ba7bb5053655d3542326a92dac5f8a9bbe
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-02 18:08:03 +0100

    Adjust MoveTaskFragment

--- app/src/main/java/com/example/howie/MoveTaskFragment.kt
+++ app/src/main/java/com/example/howie/MoveTaskFragment.kt
@@ -3,14 +3,16 @@ package com.example.howie
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.lifecycle.Observer
-import com.google.android.material.textfield.TextInputEditText
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
+@ExperimentalCoroutinesApi
 class MoveTaskFragment : DialogFragment() {
     private lateinit var listener: MoveTaskFragmentListener
 
@@ -35,23 +37,26 @@ class MoveTaskFragment : DialogFragment() {
                     dialog.dismiss()
                 }
         val dialog = messageBoxBuilder.create()
-        taskManager.taskLists.observe(this, Observer {
-            val spinner: Spinner = this.dialog!!.findViewById(R.id.task_list_spinner)
-            val nameList = mutableListOf<String>()
-            it.map { taskList -> taskList.name }
-            for (taskList in it) {
-                if (taskList.id != taskManager.currentTaskListId) {
-                    nameList.add(taskList.name)
-                    taskListIds.add(taskList.id)
+        val thisdialog = this.dialog
+        lifecycleScope.launch {
+            taskManager.taskLists.collect {
+                val spinner: Spinner = thisdialog!!.findViewById(R.id.task_list_spinner)
+                val nameList = mutableListOf<String>()
+                it.map { taskList -> taskList.name }
+                for (taskList in it) {
+                    if (taskList.id != taskManager.currentTaskListId) {
+                        nameList.add(taskList.name)
+                        taskListIds.add(taskList.id)
+                    }
                 }
+                val adapter = ArrayAdapter<String>(
+                    activity!!,
+                    android.R.layout.simple_spinner_item,
+                    nameList
+                )
+                spinner.adapter = adapter
             }
-            val adapter = ArrayAdapter<String>(
-                activity!!,
-                android.R.layout.simple_spinner_item,
-                nameList
-            )
-            spinner.adapter = adapter
-        })
+        }
         return dialog
     }
 
