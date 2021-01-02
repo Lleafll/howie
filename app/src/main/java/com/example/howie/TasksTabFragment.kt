87d0c942b8e8dbec04ed982169b61e7817257753
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-02 18:08:32 +0100

    Adjust TasksTabFragment

--- app/src/main/java/com/example/howie/TasksTabFragment.kt
+++ app/src/main/java/com/example/howie/TasksTabFragment.kt
@@ -1,22 +1,23 @@
 package com.example.howie
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.android.synthetic.main.expandable_task_list_view.*
-import kotlinx.android.synthetic.main.expandable_task_list_view.view.*
 import kotlinx.android.synthetic.main.fragment_tasks_object.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
+@ExperimentalCoroutinesApi
 class TasksTabFragment : Fragment(R.layout.fragment_tasks_tab) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewPager: ViewPager2 = view.findViewById(R.id.pager)
@@ -25,21 +26,24 @@ class TasksTabFragment : Fragment(R.layout.fragment_tasks_tab) {
         TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
         val taskManager = TaskManager.getInstance(view.context)
         val observerFactory = { position: Int, lowerText: String ->
-            Observer { tasks: List<Task> ->
+            { tasks: List<Task> ->
                 val upperText = if (tasks.isNotEmpty()) tasks.size.toString() else "✓"
                 tabLayout.getTabAt(position)!!.text = "$upperText\n$lowerText"
             }
         }
-        taskManager.doTasks.observe(this, observerFactory(0, "Do"))
-        taskManager.decideTasks.observe(this, observerFactory(1, "Decide"))
-        taskManager.delegateTasks.observe(this, observerFactory(2, "Delegate"))
-        taskManager.dropTasks.observe(this, observerFactory(3, "Drop"))
-        taskManager.lastInsertedTaskCategory.observe(this, Observer {
-            tabLayout.getTabAt(it.ordinal)?.select()
-        })
+        lifecycleScope.launch {
+            taskManager.doTasks.collect { observerFactory(0, "Do")(it) }
+            taskManager.decideTasks.collect { observerFactory(1, "Decide")(it) }
+            taskManager.delegateTasks.collect { observerFactory(2, "Delegate")(it) }
+            taskManager.dropTasks.collect { observerFactory(3, "Drop")(it) }
+            taskManager.lastInsertedTaskCategory.collect {
+                tabLayout.getTabAt(it.ordinal)?.select()
+            }
+        }
     }
 }
 
+@ExperimentalCoroutinesApi
 class TasksTabAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 4
 
@@ -52,6 +56,7 @@ class TasksTabAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     }
 }
 
+@ExperimentalCoroutinesApi
 class TasksObjectFragment : Fragment(R.layout.fragment_tasks_object) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val taskManager = TaskManager.getInstance(activity!!.applicationContext)
@@ -76,7 +81,7 @@ class TasksObjectFragment : Fragment(R.layout.fragment_tasks_object) {
 
     private fun setupView(
         view: ExpandableTasksView,
-        tasks: LiveData<List<Task>>,
+        tasks: Flow<List<Task>>,
         headerText: String,
         defaultExpandState: Boolean
     ) {
@@ -87,15 +92,17 @@ class TasksObjectFragment : Fragment(R.layout.fragment_tasks_object) {
             startActivity(intent)
         }
         view.setAdapter(taskAdapter)
-        tasks.observe(this, Observer {
-            if (it.isEmpty()) {
-                view.visibility = View.GONE
-            } else {
-                view.visibility = View.VISIBLE
-                view.setExpanded(defaultExpandState)
-                it.let { taskAdapter.submitList(it) }
+        lifecycleScope.launch {
+            tasks.collect {
+                if (it.isEmpty()) {
+                    view.visibility = View.GONE
+                } else {
+                    view.visibility = View.VISIBLE
+                    view.setExpanded(defaultExpandState)
+                    it.let { taskAdapter.submitList(it) }
+                }
             }
-        })
+        }
     }
 }
 
