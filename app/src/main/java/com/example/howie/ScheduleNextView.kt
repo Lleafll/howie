69673948ddca5b418c726be3c46ce0b8d1fa0de9
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-03 23:04:44 +0100

    Add simple ScheduleNextView

---
+++ app/src/main/java/com/example/howie/ScheduleNextView.kt
@@ -0,0 +1,9 @@
+package com.example.howie
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ScheduleNextView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    View(context, attrs, defStyleAttr) {
+}
