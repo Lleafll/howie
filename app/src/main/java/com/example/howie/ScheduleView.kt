182fadbcd12eda3b6e3582d800993e67678ef7bf
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-04 12:41:43 +0100

    Rename ScheduleNextView to ScheduleView

--- app/src/main/java/com/example/howie/ScheduleView.kt
+++ app/src/main/java/com/example/howie/ScheduleView.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
-class ScheduleNextView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+class ScheduleView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     View(context, attrs, defStyleAttr) {
 }
