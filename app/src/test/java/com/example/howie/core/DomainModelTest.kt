7b5f9af9cb24984acbf3802eb448d11856f79815
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-08 21:37:29 +0100

    Add test

---
+++ app/src/test/java/com/example/howie/core/DomainModelTest.kt
@@ -0,0 +1,13 @@
+package com.example.howie.core
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class DomainModelTest {
+
+    @Test
+    fun `getTaskListInformation return empty list when tasks are empty`() {
+        val model = DomainModel(listOf())
+        assertTrue(model.getTaskListInformation().isEmpty())
+    }
+}
