4a76481de2cdb5365d8219bab84ccf1a1f106557
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-14 22:22:58 +0100

    Add TasksRepositoryTest

---
+++ app/src/test/java/com/example/howie/ui/TasksRepositoryTest.kt
@@ -0,0 +1,39 @@
+package com.example.howie.ui
+
+import io.mockk.MockKAnnotations
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class TasksRepositoryTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        MockKAnnotations.init(this)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+        testScope.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `deleteTask saves to database`() {
+
+    }
+}
