5f787b4d5376a53e87489866a865efa70fbdba2c
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-01-15 23:29:03 +0100

    Improve TaskAdapter comparator and ViewHolder creation

--- app/src/main/java/com/example/howie/ui/TaskAdapter.kt
+++ app/src/main/java/com/example/howie/ui/TaskAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.howie.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.howie.R
 import com.example.howie.core.TaskIndex
+import com.example.howie.databinding.TaskRowItemBinding
 
 class TaskAdapter(private val adapterListener: Listener) :
     ListAdapter<TaskItemFields, TaskAdapter.TaskViewHolder>(TasksComparator()) {
@@ -24,28 +23,31 @@ class TaskAdapter(private val adapterListener: Listener) :
         fun onEditClicked(index: TaskIndex)
     }
 
-    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val taskItem: TaskItem = view.findViewById(R.id.taskItem)
-
-        companion object {
-            fun create(parent: ViewGroup): TaskViewHolder {
-                val view: View = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.task_row_item, parent, false)
-                return TaskViewHolder(view)
-            }
-        }
+    class TaskViewHolder(binding: TaskRowItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val taskItem: TaskItem = binding.taskItem
     }
 
     class TasksComparator : DiffUtil.ItemCallback<TaskItemFields>() {
-        override fun areItemsTheSame(oldItem: TaskItemFields, newItem: TaskItemFields): Boolean =
-            oldItem === newItem
+        override fun areItemsTheSame(oldItem: TaskItemFields, newItem: TaskItemFields): Boolean {
+            return oldItem.index == newItem.index
+        }
 
-        override fun areContentsTheSame(oldItem: TaskItemFields, newItem: TaskItemFields): Boolean =
-            oldItem == newItem
+        override fun areContentsTheSame(oldItem: TaskItemFields, newItem: TaskItemFields): Boolean {
+            return oldItem.name == newItem.name &&
+                    oldItem.due == newItem.due &&
+                    oldItem.snoozed == newItem.snoozed
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder =
-        TaskViewHolder.create(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder(
+            TaskRowItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val taskItemFields = getItem(position)
